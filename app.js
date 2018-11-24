9575aaea1726c39a4c8f6de602b7dd7a2aa5fd00
ben sturley <bensturley@gmail.com>
2018-11-24 20:05:44 +0000

    Favicon image files and static link rel tags in head

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError   = require('http-errors');
+var express       = require('express');
+var path          = require('path');
+var cookieParser  = require('cookie-parser');
+var logger        = require('morgan');
+var servestatic   = require('serve-static');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// statics
+// app.use(servestatic(path.join(__dirname, 'public', 'images')));
+app.use(servestatic('public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
